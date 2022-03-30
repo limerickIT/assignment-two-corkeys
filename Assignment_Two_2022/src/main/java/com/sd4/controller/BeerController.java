004a4fed68b88721bdf39edfd1bac4d277756f25
Jordan <jordan.s.j.cork@sky.com>
2022-03-30 17:23:01 +0100

    MediaType.APPLICATION_XML_VALUE was making problems

--- Assignment_Two_2022/src/main/java/com/sd4/controller/BeerController.java
+++ Assignment_Two_2022/src/main/java/com/sd4/controller/BeerController.java
@@ -1,37 +1,25 @@
 package com.sd4.Controller;
 
-import static antlr.Utils.error;
-import com.sd4.model.Brewery;
 import com.sd4.model.Beer;
 import com.sd4.service.BeerService;
 import com.sd4.service.BreweryService;
-import java.util.Collections;
 
 import java.util.List;
 import java.util.Optional;
-import static org.apache.coyote.http11.Constants.a;
-import static org.aspectj.bridge.MessageUtil.error;
-import static org.aspectj.bridge.MessageUtil.error;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
-
+//import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
 @RestController
 @Validated 
@@ -75,7 +63,7 @@ public class BeerController {
 ////        return BeerService.findAll();
 ////        
 ////    }     
-    @GetMapping(value = "/hateoas/getall",produces = {MediaType.APPLICATION_JSON_VALUE})
+    @GetMapping(value = "/hateoas/getall",produces =MediaType.APPLICATION_JSON_VALUE)
 
     public ResponseEntity<List<Beer>> getAll() {
         List<Beer> alist = BeerService.findAll();
@@ -85,13 +73,13 @@ public class BeerController {
         return new ResponseEntity(HttpStatus.NOT_FOUND);
         
         }else{
-       // Link selfLink = linkTo(methodOn(BeerController.class).getOne(Beer)).withselfRel();    
-       // a.get().add(selfLink);
+      // Link selfLink = linkTo(methodOn(BeerController.class).getOne(Beer)).withselfRel();    
+        //a.get().add(selfLink);
         return ResponseEntity.ok(alist);
              }
         }
 
-    @GetMapping(value = "/beers/{id}" ,produces = {MediaType.APPLICATION_JSON_VALUE})
+    @GetMapping(value = "/beers/{id}" ,produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Beer> getOne(@PathVariable long id) {
        Optional<Beer> o =  BeerService.findOne(id);
        
@@ -119,40 +107,11 @@ public class BeerController {
         return new ResponseEntity(HttpStatus.CREATED);
     }
     
-    @PutMapping(value = "/beers/edit", produces = {MediaType.APPLICATION_JSON_VALUE})
+    @PutMapping(value = "/beers/edit", consumes = {MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity edit(@RequestBody Beer a) { //the edit method should check if the Beer object is already in the DB before attempting to save it.
         BeerService.saveBeer(a);
         return new ResponseEntity(HttpStatus.OK);
     }
     
-    
-    /////////////////////////////////////////////////////////////////////
-//    @GetMapping(value = "/beers/drill", produces = {MediaType.APPLICATION_XML_VALUE,MediaType.APPLICATION_JSON_VALUE})
-//    public ResponseEntity drill(@RequestBody Beer a) { 
-//        BeerService.drillBeer(a);
-//        return new ResponseEntity(HttpStatus.OK);
-//    }
-//    
-//    @GetMapping(value = "/beers/map", produces = {MediaType.APPLICATION_XML_VALUE,MediaType.APPLICATION_JSON_VALUE})
-//    public ResponseEntity map(@RequestBody Beer a) { 
-//        BeerService.mapBeer(a);
-//        return new ResponseEntity(HttpStatus.OK);
-//    }
-//    
-//    @GetMapping(value = "/beers/pdf", produces = {MediaType.APPLICATION_XML_VALUE,MediaType.APPLICATION_JSON_VALUE})
-//    public ResponseEntity pdf (@RequestBody Beer a) { 
-//        BeerService.pdfBeer(a);
-//        return new ResponseEntity(HttpStatus.OK);
-//    }
-//    
-//    @GetMapping(value = "/beers/qr", produces = {MediaType.APPLICATION_XML_VALUE,MediaType.APPLICATION_JSON_VALUE})
-//    public ResponseEntity qr (@RequestBody Beer a) { 
-//        BeerService.qrBeer(a);
-//        return new ResponseEntity(HttpStatus.OK);
-//    }
-//    @GetMapping(value = "/beers/zip", produces = {MediaType.APPLICATION_XML_VALUE,MediaType.APPLICATION_JSON_VALUE})
-//    public ResponseEntity zip(@RequestBody Beer a) { 
-//        BeerService.zipBeer(a);
-//        return new ResponseEntity(HttpStatus.OK);
-//    }
+
 }
--- Assignment_Two_2022/src/main/java/com/sd4/controller/BreweryController.java
+++ Assignment_Two_2022/src/main/java/com/sd4/controller/BreweryController.java
@@ -5,6 +5,20 @@
  */
 package com.sd4.controller;
 
+import com.sd4.model.Brewery;
+import com.sd4.service.BreweryService;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -13,5 +27,62 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class BreweryController {
+       @Autowired
+    private BreweryService BreweryService;
+    //method to load the add brewery form. This method also creates a Brewery object that will back the add brewery form    
     
+     @Autowired
+      private BreweryService Brewerieservice;
+   
+
+  
+    @GetMapping(value = "/hateoas/getallbrewery",produces =MediaType.APPLICATION_JSON_VALUE)
+
+    public ResponseEntity<List<Brewery>> getAll() {
+        List<Brewery> alist = BreweryService.findAll();
+       
+        if(alist.isEmpty()){
+        
+        return new ResponseEntity(HttpStatus.NOT_FOUND);
+        
+        }else{
+      // Link selfLink = linkTo(methodOn(BreweryController.class).getOne(Brewery)).withselfRel();    
+        //a.get().add(selfLink);
+        return ResponseEntity.ok(alist);
+             }
+        }
+
+    @GetMapping(value = "/brewerys/{id}" ,produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Brewery> getOne(@PathVariable long id) {
+       Optional<Brewery> o =  BreweryService.findOne(id);
+       
+       if (!o.isPresent()){ 
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+            }else{ 
+            return ResponseEntity.ok(o.get());
+       }
+    }
+    
+    @GetMapping(value = "/brewerys/count", produces = {MediaType.APPLICATION_JSON_VALUE})
+    public long getCount() {
+        return BreweryService.count();
+    }
+    
+    @DeleteMapping(value = "/brewerys/{id}", produces = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity delete(@PathVariable long id) {
+        BreweryService.deleteByID(id);
+        return new ResponseEntity(HttpStatus.OK);
+    }
+    
+    @PostMapping(value = "/brewerys/add", consumes = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity add(@RequestBody Brewery a) {
+        BreweryService.saveBrewery(a);
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+    
+    @PutMapping(value = "/brewerys/edit", consumes = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity edit(@RequestBody Brewery a) { //the edit method should check if the Brewery object is already in the DB before attempting to save it.
+        BreweryService.saveBrewery(a);
+        return new ResponseEntity(HttpStatus.OK);
+    }
 }
